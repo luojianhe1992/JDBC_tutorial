b7b2d9e78562213548ec1675d8e120ef6b9cef68
luojianhe1992 <luojianhe1992@gmail.com>
2016-03-26 14:11:15 -0400

    Finish implementing the test_6, calling stored procedure using jdbc.

--- test_6/src/test_6/IncreaseSalariesForDepartment.java
+++ test_6/src/test_6/IncreaseSalariesForDepartment.java
@@ -11,7 +11,7 @@ import com.mysql.jdbc.PreparedStatement;
 
 public class IncreaseSalariesForDepartment {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		Connection myConnection = null;
 		java.sql.CallableStatement myStatment = null;
@@ -28,18 +28,30 @@ public class IncreaseSalariesForDepartment {
 			String department = "Engineering";
 			int increment = 10000;
 			
+			System.out.println("Before calling the stored procedure.");
+			showSalaries(myConnection, department);
 			
-			
+			// call precedure in the sql
 			String sql = "{call increase_salaries_for_department(?, ?)}";
-			
 			myStatment = myConnection.prepareCall(sql);
 			
 			myStatment.setString(1, department);
 			myStatment.setInt(2, increment);
 				
+			System.out.println("Is going to call stored precedure, increase_salaries_for_department, "
+					+ "and set department to be Engineering, increment to be 10000.");
+			
+			myStatment.execute();
+			System.out.println("Finish calling stored procedure.");
+			
+			System.out.println("After calling the procedures.");
+			showSalaries(myConnection, department);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+		finally {
+			close(myConnection, myStatment, null);
 		}
 	}
 	
@@ -60,25 +72,23 @@ public class IncreaseSalariesForDepartment {
 			
 			myResultSet = myStatement.executeQuery();
 			
+			System.out.println("*********************************************");
 			while (myResultSet.next()) {
-				
 				String the_first_name = myResultSet.getString("first_name");
 				String the_last_name = myResultSet.getString("last_name");
 				String the_email = myResultSet.getString("email");
 				String the_department = myResultSet.getString("department");
-				double the_salaries = myResultSet.getDouble("salaries");
-				
+				double the_salaries = myResultSet.getDouble("salary");
 				System.out.println(the_first_name + "\t"
 								+ the_last_name + "\t"
 								+ the_email + "\t"
 								+ the_department + "\t"
 								+ the_salaries);
-				
 			}
-			
+			System.out.println("*********************************************");
 			
 		} catch (SQLException e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 		finally {
 			close(myStatement, myResultSet);
@@ -87,15 +97,12 @@ public class IncreaseSalariesForDepartment {
 	
 	// define close function
 	private static void close(Connection myConnection, Statement myStatment, ResultSet myResultSet) throws SQLException{
-		
 		if (myResultSet != null) {
 			myResultSet.close();
 		}
-		
 		if (myStatment != null) {
 			myStatment.close();
 		}
-		
 		if (myConnection != null) {
 			myConnection.close();
 		}
@@ -103,14 +110,11 @@ public class IncreaseSalariesForDepartment {
 	
 	// overload the close function
 	private static void close(Statement myStatement, ResultSet myResultSet) throws SQLException{
-		
 		if (myStatement != null) {
 			myStatement.close();
 		}
-		
 		if (myResultSet != null) {
 			myResultSet.close();
 		}
-		
 	}
 }
