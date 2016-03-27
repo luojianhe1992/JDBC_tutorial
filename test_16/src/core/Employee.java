76ee2496d252fae95cab1edef1d666cc70b34632
luojianhe1992 <luojianhe1992@gmail.com>
2016-03-26 21:37:41 -0400

    Add a new project test_16

---
+++ test_16/src/core/Employee.java
@@ -0,0 +1,82 @@
+package core;
+
+import java.math.BigDecimal;
+
+public class Employee {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private BigDecimal salary;
+	
+	
+	public Employee(int id, String firstName, String lastName, String email, BigDecimal salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.salary = salary;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public BigDecimal getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", salary=" + salary + "]";
+	}
+	
+	
+	
+}
